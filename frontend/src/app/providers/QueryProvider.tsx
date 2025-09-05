575da2c588cd38fdc774921e8113ea4df525475c
yoownny <kelosy00@naver.com>
2025-09-05 11:10:43 +0900

    feat(fe): qeury 설정

--- frontend/src/app/providers/QueryProvider.tsx
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -1,3 +1,4 @@
+// 전역으로 queryClient(데이터 캐시/관리 객체)를 공금
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../query/client";
 
--- frontend/src/app/query/client.ts
+++ frontend/src/app/query/client.ts
@@ -3,10 +3,10 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60_000,
-      gcTime: 300_000,
-      retry: 1,
-      refetchOnWindowFocus: false,
+      staleTime: 60_000, // 데이터를 얼마나 오래 신선하다고 볼지 (1분)
+      gcTime: 300_000, // 캐시 데이터를 얼마나 오래 보관할지 (5분)
+      retry: 1, // 요청 실패 시 재시도 횟수
+      refetchOnWindowFocus: false, // 브라우저 창을 다시 클릭했을 떄 자동 재요청 여부
     },
   },
 });
