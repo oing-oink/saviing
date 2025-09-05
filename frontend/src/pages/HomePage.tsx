7ce6e53ed9ec1ee2b038b8fdd9c526d3e23aae7f
yoownny <kelosy00@naver.com>
2025-09-05 11:07:38 +0900

    feat(fe): tailwind 설치

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/shared/components/ui/button";
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
