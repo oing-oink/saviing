5b3b8643b43b066cbe64d2c23680e25fe1fe3275
yoownny <kelosy00@naver.com>
2025-09-07 22:26:05 +0900

    chore(fe): Eslint 설정 추가

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "node_modules"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
@@ -21,6 +21,14 @@ export default tseslint.config([
     },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "no-implicit-coercion": "error", // 타입 변환은 명시적으로
+      curly: ["error", "all"], // 모든 제어문에 중괄호 강제
+      "no-warning-comments": [
+        "warn",
+        { terms: ["todo", "fixme", "bug"], location: "start" },
+      ], // TODO/BUG는 경고로 추적
+      "react-hooks/exhaustive-deps": "warn", // Hooks 의존성 검사
+      "react-refresh/only-export-components": "warn", // Fast Refresh 최적화
     },
   },
 ]);
