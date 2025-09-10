f90178e2b502be5806c71fad4da89061e825dcc2
홍혜린 <shift6210@gmail.com>
2025-09-10 13:14:36 +0900

    refac(fe): mock data를 별도 파일로 분리

---
+++ frontend/src/features/game/shop/mocks/inventoryMockData.ts
@@ -0,0 +1,82 @@
+import type { Item } from '@/features/game/shop/types/item';
+
+export const mockInventoryItems: Item[] = [
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+  {
+    id: 1,
+    name: 'sample',
+    image: 'assets/mouse_item.png',
+    category: '냥이',
+  },
+];
--- frontend/src/pages/ShopPage.tsx
+++ frontend/src/pages/ShopPage.tsx
@@ -1,19 +1,11 @@
 import Inventory from '@/features/game/shop/components/inventory';
+import { mockInventoryItems } from '@/features/game/shop/mocks/inventoryMockData';
 
 const ShopPage = () => {
   return (
     <div className="font-galmuri">
       <div>Shop Page</div>
-      <Inventory
-        items={[
-          {
-            id: 1,
-            name: 'sample',
-            image: 'assets/mouse_item.png',
-            category: '냥이',
-          },
-        ]}
-      />
+      <Inventory items={mockInventoryItems} />
     </div>
   );
 };
