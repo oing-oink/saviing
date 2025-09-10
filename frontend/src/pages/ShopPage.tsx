5a919760f4b8ac01f76397dadae2d704b9387120
홍혜린 <shift6210@gmail.com>
2025-09-10 16:32:06 +0900

    refac(fe): 오타 수정 및 tailwindcss 적용

--- frontend/src/pages/ShopPage.tsx
+++ frontend/src/pages/ShopPage.tsx
@@ -1,6 +1,6 @@
 import Inventory from '@/features/game/shop/components/inventory';
 import { mockInventoryItems } from '@/features/game/shop/mocks/inventoryMockData';
-import GameMomey from '@/assets/game_button/gameMoney.png';
+import GameMoney from '@/assets/game_button/gameMoney.png';
 import backButton from '@/assets/game_button/backButton.png';
 import gachaButton from '@/assets/game_button/gachaButton.png';
 import sampleRoom from '@/assets/sampleRoom.png';
@@ -13,29 +13,17 @@ const ShopPage = () => {
     <div className="game min-h-screen w-full bg-store-bg font-galmuri">
       <div className="flex items-center justify-between px-6">
         <button onClick={() => navigate(PAGE_PATH.GAME)}>
-          <img
-            className="pt-10"
-            src={backButton}
-            alt=""
-            style={{ width: '2.1rem' }}
-          />
+          <img className="w-8 pt-10" src={backButton} alt="" />
         </button>
-        <div className="relative" style={{ width: '50%', height: '50%' }}>
-          <img src={GameMomey} alt="" className="h-full w-full pt-10" />
-          <div
-            className="absolute pt-10"
-            style={{ left: '2rem', top: '50%', transform: 'translateY(-50%)' }}
-          >
+        <div className="relative h-1/2 w-1/2">
+          <img src={GameMoney} alt="" className="h-full w-full pt-10" />
+          <div className="absolute top-1/2 left-8 -translate-y-1/2 pt-10">
             {/*TODO: 실제 코인 보유량으로 전환 */}
             <span className="text-md font-bold text-white">1000</span>
           </div>
           <div
-            className="absolute pt-10"
-            style={{
-              left: '7.8rem',
-              top: '50%',
-              transform: 'translateY(-50%)',
-            }}
+            className="absolute top-1/2 -translate-y-1/2 pt-10"
+            style={{ left: '7.8rem' }}
           >
             <span className="text-md font-bold text-white">1000</span>
           </div>
@@ -45,12 +33,7 @@ const ShopPage = () => {
             navigate(PAGE_PATH.GACHA);
           }}
         >
-          <img
-            className="pt-10"
-            src={gachaButton}
-            alt=""
-            style={{ width: '2.1rem' }}
-          />
+          <img className="w-8 pt-10" src={gachaButton} alt="" />
         </button>
       </div>
       <img src={sampleRoom} alt="" className="mt-10" />
