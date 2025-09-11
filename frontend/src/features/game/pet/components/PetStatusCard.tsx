393e552459ecc52c5be37eb0790fcd5f8c69e7b4
kelosy00 <kelosy00@naver.com>
2025-09-11 18:47:22 +0900

    feat(fe): popover 설정 (S13P21A309-66)

--- frontend/src/features/game/pet/components/PetStatusCard.tsx
+++ frontend/src/features/game/pet/components/PetStatusCard.tsx
@@ -6,95 +6,77 @@ import { usePetStore } from '@/features/game/pet/store/usePetStore';
 import { usePetStatusCard } from '@/features/game/pet/hooks/usePetStatusCard';
 import feed from '@/assets/game_pet/feed.png';
 import play from '@/assets/game_pet/play.png';
-import { X } from 'lucide-react';
-import { Loader2 } from 'lucide-react'; // shadcn에서 자주 쓰는 로딩 아이콘
+import { Loader2 } from 'lucide-react';
 
 const PetStatusCard = () => {
-  const { closeStatusCard, inventory } = usePetStore();
+  const { inventory } = usePetStore();
   //TODO: api 작성 후 petId로 수정
   const { petData, isLoading, error, levelClass } = usePetStatusCard(1);
 
-  const handleCardClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-  };
+  if (isLoading) {
+    return (
+      <Card className="flex min-h-64 items-center justify-center rounded-t-2xl p-5">
+        <div className="flex h-full items-center justify-center">
+          <Loader2 className="h-6 w-6 animate-spin text-muted-foreground" />
+          <span className="ml-2 text-sm text-muted-foreground">
+            불러오는 중...
+          </span>
+        </div>
+      </Card>
+    );
+  }
+
+  if (error || !petData) {
+    return (
+      <Card className="flex min-h-64 items-center justify-center rounded-t-2xl p-5">
+        <div className="flex h-full items-center justify-center">
+          <div className="text-red-500">데이터를 불러올 수 없습니다</div>
+        </div>
+      </Card>
+    );
+  }
 
   return (
-    <div className="absolute right-0 bottom-0 left-0 animate-in duration-300 slide-in-from-bottom">
-      <Card
-        className="mx-6 mb-10 h-[16rem] rounded-t-2xl p-5"
-        onClick={handleCardClick}
-      >
-        {isLoading && (
-          <div className="flex items-center justify-center">
-            <Loader2 className="h-6 w-6 animate-spin text-muted-foreground" />
-            <span className="ml-2 text-sm text-muted-foreground">
-              불러오는 중...
-            </span>
-          </div>
-        )}
-
-        {(error || !petData) && !isLoading && (
-          <div className="flex items-center justify-between">
-            <div className="text-red-500">데이터를 불러올 수 없습니다</div>
-            <button onClick={closeStatusCard}>
-              <X />
-            </button>
-          </div>
-        )}
-
-        {petData && !isLoading && !error && (
-          <>
-            <div className="flex items-center gap-3">
-              <Badge className={`${levelClass}`}>Lv{petData.level}</Badge>
-              <div>{petData.name}</div>
-              <button onClick={closeStatusCard} className="ml-auto">
-                <X />
-              </button>
-            </div>
-            <div className="space-y-4">
-              <div className="flex gap-4">
-                <StatusProgress
-                  label="포만감"
-                  value={petData.energy}
-                  maxValue={petData.maxEnergy}
-                  className="progress-energy"
-                />
-                <StatusProgress
-                  label="애정도"
-                  value={petData.affection}
-                  maxValue={petData.maxAffection}
-                  className="progress-affection"
-                />
-              </div>
-              <div>
-                <StatusProgress
-                  label="경험치"
-                  value={petData.exp}
-                  maxValue={petData.requiredExp}
-                  className="progress-experience"
-                />
-              </div>
-            </div>
-            <div className="flex justify-center gap-5">
-              <Button
-                variant="secondary"
-                className="h-20 flex-1 flex-col gap-2"
-              >
-                <img src={feed} className="h-8 w-8" />
-                {inventory.feed}개
-              </Button>
-              <Button
-                variant="secondary"
-                className="h-20 flex-1 flex-col gap-2"
-              >
-                <img src={play} className="h-8 w-8" />
-                {inventory.toy}개
-              </Button>
-            </div>
-          </>
-        )}
-      </Card>
-    </div>
+    <Card className="min-h-64 rounded-t-2xl p-5">
+      <div className="flex items-center gap-3">
+        <Badge className={`${levelClass}`}>Lv{petData.level}</Badge>
+        <div>{petData.name}</div>
+      </div>
+      <div className="space-y-4">
+        <div className="flex gap-4">
+          <StatusProgress
+            label="포만감"
+            value={petData.energy}
+            maxValue={petData.maxEnergy}
+            className="progress-energy"
+          />
+          <StatusProgress
+            label="애정도"
+            value={petData.affection}
+            maxValue={petData.maxAffection}
+            className="progress-affection"
+          />
+        </div>
+        <div>
+          <StatusProgress
+            label="경험치"
+            value={petData.exp}
+            maxValue={petData.requiredExp}
+            className="progress-experience"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center gap-5">
+        <Button variant="secondary" className="h-20 flex-1 flex-col gap-2">
+          <img src={feed} className="h-8 w-8" />
+          {inventory.feed}개
+        </Button>
+        <Button variant="secondary" className="h-20 flex-1 flex-col gap-2">
+          <img src={play} className="h-8 w-8" />
+          {inventory.toy}개
+        </Button>
+      </div>
+    </Card>
   );
 };
 
--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,37 +1,44 @@
 import PetHud from '@/features/game/pet/components/PetHud';
 import PetStatusCard from '@/features/game/pet/components/PetStatusCard';
-import { usePetStore } from '@/features/game/pet/store/usePetStore';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverAnchor,
+} from '@/shared/components/ui/popover';
 import groom1 from '@/assets/game_bg/groom1.png';
 import groom2 from '@/assets/game_bg/groom2.png';
 
 const GamePage = () => {
-  const { isStatusCardOpen, closeStatusCard, openStatusCard } = usePetStore();
-
   return (
-    <div className="game relative flex h-screen flex-col overflow-hidden bg-sky-bg font-galmuri">
-      <img src={groom1} className="absolute top-20 left-1 h-30 w-50" />
+    <div className="game relative flex h-screen flex-col bg-sky-bg font-galmuri">
+      {/* <img src={groom1} className="absolute top-20 left-1 h-30 w-50" />
       <img src={groom2} className="absolute top-60 left-80 h-20 w-80" />
       <img src={groom1} className="absolute top-100 left-2 h-40 w-80" />
-      <img src={groom2} className="absolute top-170 left-64 h-30 w-50" />
+      <img src={groom2} className="absolute top-170 left-64 h-30 w-50" /> */}
 
-      <div className="relative z-30 px-3">
+      <div className="px-3">
         <PetHud />
       </div>
-      {isStatusCardOpen && (
-        <div className="absolute inset-0 z-10" onClick={closeStatusCard}></div>
-      )}
 
-      <div className="flex flex-1 items-center justify-center">
-        <div className="z-30 cursor-pointer text-6xl" onClick={openStatusCard}>
-          🐱
-        </div>
+      <div className="flex w-full flex-1 items-center justify-center">
+        <Popover>
+          <PopoverTrigger asChild>
+            <div className="cursor-pointer text-6xl">🐱</div>
+          </PopoverTrigger>
+
+          <PopoverAnchor className="absolute right-0 bottom-0 left-0" />
+
+          <PopoverContent
+            side="top"
+            align="center"
+            sideOffset={16}
+            className="w-screen max-w-sm bg-transparent p-0"
+          >
+            <PetStatusCard />
+          </PopoverContent>
+        </Popover>
       </div>
-
-      {isStatusCardOpen && (
-        <div className="z-30">
-          <PetStatusCard />
-        </div>
-      )}
     </div>
   );
 };
