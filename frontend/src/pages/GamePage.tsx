ef2a1ebe00052cd262dcaf1003ffd5d390cfc1b0
kelosy00 <kelosy00@naver.com>
2025-09-12 13:18:48 +0900

    feat(fe): 구름 배경 추가 (S13P21A309-66)

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -6,38 +6,43 @@ import {
   PopoverTrigger,
   PopoverAnchor,
 } from '@/shared/components/ui/popover';
+import Cloud from '@/features/game/shared/components/Cloud';
 import groom1 from '@/assets/game_bg/groom1.png';
 import groom2 from '@/assets/game_bg/groom2.png';
 
 const GamePage = () => {
   return (
-    <div className="game relative flex h-screen flex-col bg-sky-bg font-galmuri">
-      {/* <img src={groom1} className="absolute top-20 left-1 h-30 w-50" />
-      <img src={groom2} className="absolute top-60 left-80 h-20 w-80" />
-      <img src={groom1} className="absolute top-100 left-2 h-40 w-80" />
-      <img src={groom2} className="absolute top-170 left-64 h-30 w-50" /> */}
-
-      <div className="px-3">
-        <PetHud />
+    <div className="game relative flex h-screen flex-col overflow-hidden font-galmuri">
+      <div className="absolute inset-0 bg-sky-bg">
+        <Cloud src={groom1} top={10} left={0} height={5} duration={30} />
+        <Cloud src={groom2} top={30} left={20} height={8} duration={65} />
+        <Cloud src={groom1} top={60} left={50} height={8} duration={40} />
+        <Cloud src={groom2} top={80} left={20} height={6} duration={25} />
       </div>
 
-      <div className="flex w-full flex-1 items-center justify-center">
-        <Popover>
-          <PopoverTrigger asChild>
-            <div className="cursor-pointer text-6xl">🐱</div>
-          </PopoverTrigger>
+      <div className="relative flex h-full flex-col">
+        <div className="px-3">
+          <PetHud />
+        </div>
 
-          <PopoverAnchor className="absolute right-0 bottom-0 left-0" />
+        <div className="flex w-full flex-1 items-center justify-center">
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className="cursor-pointer text-6xl">🐱</div>
+            </PopoverTrigger>
 
-          <PopoverContent
-            side="top"
-            align="center"
-            sideOffset={16}
-            className="w-screen max-w-sm bg-transparent p-0"
-          >
-            <PetStatusCard />
-          </PopoverContent>
-        </Popover>
+            <PopoverAnchor className="absolute right-0 bottom-0 left-0" />
+
+            <PopoverContent
+              side="top"
+              align="center"
+              sideOffset={24}
+              className="w-screen max-w-sm p-0"
+            >
+              <PetStatusCard />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
     </div>
   );
