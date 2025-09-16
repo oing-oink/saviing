9b5e9be74d71c293121df435cb213b59d56ca1b9
홍혜린 <shift6210@gmail.com>
2025-09-16 14:33:52 +0900

    feat(fe): 레이아웃 파일 작성

---
+++ frontend/src/features/savings/components/SavingsLayout.tsx
@@ -0,0 +1,15 @@
+import TopBar from './TopBar';
+import BottomNav from './BottomNav';
+import { Outlet } from 'react-router-dom';
+
+export default function SavingsLayout() {
+  return (
+    <div className="mx-auto min-h-dvh w-full max-w-md bg-violet-50">
+      <TopBar />
+      <div className="pt-16 pb-20">
+        <Outlet />
+      </div>
+      <BottomNav />
+    </div>
+  );
+}
