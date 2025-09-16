551fc49a705848ba578694ec67444a68a57f3984
홍혜린 <shift6210@gmail.com>
2025-09-16 12:58:33 +0900

    feat(fe): 홈 페이지 작성

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,27 @@
-import { Button } from '@/shared/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import BottomNav from '@/features/savings/components/BottomNav';
+import PetCard from '@/features/savings/components/PetCard';
+import PromoCarousel from '@/features/savings/components/PromoCarousel';
+import SavingCard from '@/features/savings/components/SavingCard';
+import TopBar from '@/features/savings/components/TopBar';
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <Button>Home</Button>
-      <button
-        onClick={() => navigate('/colortest')}
-        className="block w-full rounded-lg bg-blue-500 p-2 text-center text-white"
-      >
-        Go to Color Test Page
-      </button>
-      <button
-        onClick={() => navigate('/game')}
-        className="game block w-full rounded-lg bg-primary p-2 text-center text-white"
-      >
-        Go to Color Game Page
-      </button>
+    <div className="flex min-h-screen flex-col bg-violet-50">
+      <TopBar />
+      <div className="flex-1 pb-20">
+        <div className="flex justify-center py-4">
+          <div className="w-[90%]">
+            <PromoCarousel />
+          </div>
+        </div>
+        <div className="flex items-center justify-center px-4">
+          <PetCard petId={1} />
+        </div>
+        <div className="flex items-center justify-center px-4">
+          <SavingCard />
+        </div>
+      </div>
+      <BottomNav />
     </div>
   );
 };
