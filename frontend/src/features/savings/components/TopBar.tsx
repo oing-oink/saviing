f7cafcc566524069becb410e1e8835ca8894a7f8
홍혜린 <shift6210@gmail.com>
2025-09-16 12:57:28 +0900

    feat(fe): 상단 네비게이션 바 제작

---
+++ frontend/src/features/savings/components/TopBar.tsx
@@ -0,0 +1,18 @@
+import { Bell } from 'lucide-react';
+
+const TopBar = () => {
+  return (
+    <header className="saving flex w-full items-center justify-between bg-violet-50 px-6 py-4">
+      {/* 왼쪽 로고 */}
+      <h1 className="text-2xl font-bold text-primary">Saviing</h1>
+
+      {/* 오른쪽 사용자 정보 + 알림 */}
+      <div className="flex items-center gap-4">
+        <span className="text-lg font-semibold text-violet-500">오익준님</span>
+        <Bell className="h-6 w-6 text-secondary" />
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
