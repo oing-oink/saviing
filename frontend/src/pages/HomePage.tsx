2b66e6249cd5bed4911530f0d570edd74308cbe2
kelosy00 <kelosy00@naver.com>
2025-09-16 12:38:10 +0900

    refac(fe): 경로지정

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/shared/components/ui/button';
 import { useNavigate } from 'react-router-dom';
+import { PAGE_PATH } from '@/shared/constants/path';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -8,13 +9,13 @@ const HomePage = () => {
     <div>
       <Button>Home</Button>
       <button
-        onClick={() => navigate('/colortest')}
+        onClick={() => navigate(PAGE_PATH.COLORTEST)}
         className="block w-full rounded-lg bg-blue-500 p-2 text-center text-white"
       >
         Go to Color Test Page
       </button>
       <button
-        onClick={() => navigate('/game')}
+        onClick={() => navigate(PAGE_PATH.GAME)}
         className="game block w-full rounded-lg bg-primary p-2 text-center text-white"
       >
         Go to Color Game Page
