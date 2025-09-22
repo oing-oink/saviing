62c3c83094f14cc477a3b06664f0651da5dc2aca
honeybutter7 <honeybutter7@naver.com>
2025-09-22 22:07:20 +0900

    fix(fe): conflict 충돌 해결 (S13P21A309-110)

--- frontend/src/features/savings/layouts/SavingsDetailLayout.tsx
+++ frontend/src/features/savings/layouts/SavingsDetailLayout.tsx
@@ -8,15 +8,12 @@ const SavingsDetailLayout = () => {
   useScrollReset();
 
   return (
-<<<<<<< HEAD
-    <div className="saving mx-auto min-h-dvh w-full max-w-md bg-secondary">
-      <DetailTopBar title={title} backMode="entry" />
-      <div>
-=======
+    // <div className="saving mx-auto min-h-dvh w-full max-w-md bg-secondary">
+    //   <DetailTopBar title={title} backMode="entry" />
+    //   <div>
     <div className="saving mx-auto flex h-dvh w-full max-w-md flex-col bg-secondary">
       <DetailTopBar />
       <PageScrollArea className="h-[calc(100dvh-56px)]">
->>>>>>> 7dee7b84714a778a7f45647f0eedf63ec6ce8d48
         <Outlet />
       </PageScrollArea>
     </div>
